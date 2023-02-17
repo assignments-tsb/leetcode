4980702db1eae00e7bd2db506945d72d7337c60d
LM Bibera <secret.transaction@gmail.com>
2023-02-17 17:26:48 +0800

    Problem 2: WIP

--- kotlin-leetcode/src/main/kotlin/dev/codefactory/leetcode/core/Problem002AddTwoNumbers.kt
+++ kotlin-leetcode/src/main/kotlin/dev/codefactory/leetcode/core/Problem002AddTwoNumbers.kt
@@ -12,10 +12,38 @@ package dev.codefactory.leetcode.core
 class Problem002AddTwoNumbers {
 
     fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
-        return ListNode(l1!!.`val` + l2!!.`val`)
+        val sum = ListNode(l1!!.`val` + l2!!.`val`)
+
+        if (l1.next!=null && l2.next!=null) {
+            sum.next = addTwoNumbers(l1.next, l2.next)
+        }
+
+        return sum
     }
 }
 
 class ListNode(var `val`: Int) {
     var next: ListNode? = null
+
+    override fun toString(): String {
+        if (next==null) return "$`val`"
+        return "$`val` -> ${next.toString()}"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other == null || other !is ListNode) {
+            return false
+        }
+
+        if (next!=null && other.next!=null) {
+            return `val` == other.`val` && next!! == other.next
+        }
+        return `val` == other.`val`
+    }
+
+    override fun hashCode(): Int {
+        var result = `val`
+        result = 31 * result + (next?.hashCode() ?: 0)
+        return result
+    }
 }
