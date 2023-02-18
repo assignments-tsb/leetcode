9e5bc9738b40df6a86714cbe0021dabc2b17d8da
LM Bibera <secret.transaction@gmail.com>
2023-02-18 11:38:35 +0800

    Problem 4: Initial solution

--- kotlin-leetcode/src/main/kotlin/dev/codefactory/leetcode/core/Solution004MedianOfTwoSortedArrays.kt
+++ kotlin-leetcode/src/main/kotlin/dev/codefactory/leetcode/core/Solution004MedianOfTwoSortedArrays.kt
@@ -3,7 +3,19 @@ package dev.codefactory.leetcode.core
 class Solution004MedianOfTwoSortedArrays {
 
     private fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
-        return 0.0
+        val newArray = ArrayList<Int>()
+        newArray.addAll(nums1.toList())
+        newArray.addAll(nums2.toList())
+        newArray.sort()
+
+        val size = newArray.size
+        val mid = size/2
+
+        if (size%2 == 0) {
+            return (newArray[mid] + newArray[mid-1]) / 2.0
+        }
+
+        return newArray[mid].toDouble()
     }
 
     //for testing purposes only
