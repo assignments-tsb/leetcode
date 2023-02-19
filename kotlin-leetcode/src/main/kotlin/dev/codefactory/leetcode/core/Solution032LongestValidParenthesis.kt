f2e8909591eb4ce0398d7612b272fd81d5d7c58c
LM Bibera <secret.transaction@gmail.com>
2023-02-19 20:43:26 +0800

    Problem 32: Add solution

--- kotlin-leetcode/src/main/kotlin/dev/codefactory/leetcode/core/Solution032LongestValidParenthesis.kt
+++ kotlin-leetcode/src/main/kotlin/dev/codefactory/leetcode/core/Solution032LongestValidParenthesis.kt
@@ -1,8 +1,29 @@
 package dev.codefactory.leetcode.core
 
+import java.util.Stack
+
 class Solution032LongestValidParenthesis {
 
     fun longestValidParentheses(s: String): Int {
-        return 0
+
+        val stack = Stack<Int>()
+        stack.push(-1)
+        var longest = 0
+
+        for (i in 0 until s.length) {
+            if (s[i] == '(') {
+                stack.push(i)
+            } else {
+                stack.pop()
+
+                if (stack.isEmpty()) {
+                    stack.push(i)
+                } else {
+                    longest = maxOf(longest, i-stack.peek())
+                }
+            }
+        }
+
+        return longest
     }
 }
