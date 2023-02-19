146d250c25dc7f04f5af841d6a2c03630e0a51a4
LM Bibera <secret.transaction@gmail.com>
2023-02-19 12:37:01 +0800

    Problem 10: Funny solution using built-in method

--- kotlin-leetcode/src/main/kotlin/dev/codefactory/leetcode/core/Solution010RegexMatching.kt
+++ kotlin-leetcode/src/main/kotlin/dev/codefactory/leetcode/core/Solution010RegexMatching.kt
@@ -3,6 +3,7 @@ package dev.codefactory.leetcode.core
 class Solution010RegexMatching {
 
     fun isMatch(s: String, p: String): Boolean {
-        return false
+        return s.matches(p.toRegex())
+    // TODO its funny that this worked, but ill try to figure out a real algorithm later
     }
 }
