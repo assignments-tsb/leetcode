1215bcb5cc3898364b589edd8eeec3173eed1e34
LM Bibera <secret.transaction@gmail.com>
2023-02-19 18:14:41 +0800

    Problem 23: Implement solution

--- kotlin-leetcode/src/main/kotlin/dev/codefactory/leetcode/core/Solution023MergeKSortedList.kt
+++ kotlin-leetcode/src/main/kotlin/dev/codefactory/leetcode/core/Solution023MergeKSortedList.kt
@@ -5,7 +5,38 @@ import dev.codefactory.leetcode.core.shared.ListNode
 class Solution023MergeKSortedList {
     private fun mergeKLists(lists: Array<ListNode?>): ListNode? {
         if (lists.isEmpty()) return null
-        return null
+
+        var remaining = lists.size - 1
+
+        while (remaining != 0) {
+            var i = 0
+            var j = remaining
+
+            while (i < j) {
+                lists[i] = mergeNodes(lists[i], lists[j])
+
+                i++
+                j--
+
+                if (i >= j) remaining = j
+            }
+        }
+
+        return lists[0]
+    }
+
+    private fun mergeNodes(a: ListNode?, b: ListNode?): ListNode? {
+        if (a==null && b==null) return null
+        if (a==null) return b
+        if (b==null) return a
+
+        if(a.`val` <= b.`val`) {
+            a.next = mergeNodes(a.next, b)
+            return a
+        }
+
+        b.next = mergeNodes(a, b.next)
+        return b
     }
 
     fun mergeKLists(listNodes: ArrayList<ListNode>): ListNode? {
