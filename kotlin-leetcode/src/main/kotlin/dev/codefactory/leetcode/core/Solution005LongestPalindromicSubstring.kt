f683a58c304a80f20d35df5f4be001a5309a8f72
LM Bibera <secret.transaction@gmail.com>
2023-02-20 22:24:47 +0800

    Problem 5: Implemented

--- kotlin-leetcode/src/main/kotlin/dev/codefactory/leetcode/core/Solution005LongestPalindromicSubstring.kt
+++ kotlin-leetcode/src/main/kotlin/dev/codefactory/leetcode/core/Solution005LongestPalindromicSubstring.kt
@@ -8,6 +8,39 @@ class Solution005LongestPalindromicSubstring {
             else s[0].toString()
         }
 
-        return s
+        val n = s.length
+        val flags = Array(n) { BooleanArray(n) }
+
+        for (i in 0 until n) {
+            flags[i][i] = true
+        }
+
+        var maxLength = 1
+        var start = 0
+
+        for (i in 0 until n-1) {
+            if (s[i] == s[i+1]) {
+                flags[i][i+1] = true
+                start = i
+                maxLength = 2
+            }
+        }
+
+        for (currentLength in 3 .. n) {
+            for (i in 0 until (n-currentLength+1)) {
+                val end = i + currentLength - 1
+
+                if (flags[i+1][end-1] && s[i] == s[end]) {
+                    flags[i][end] = true
+
+                    if (currentLength > maxLength) {
+                        start = i
+                        maxLength = currentLength
+                    }
+                }
+            }
+        }
+
+        return s.substring(start, start+maxLength)
     }
 }
