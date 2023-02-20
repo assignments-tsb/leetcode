6eb5c32a63e4afb21a867a0aef98fbcd56ad14b3
LM Bibera <secret.transaction@gmail.com>
2023-02-20 22:39:47 +0800

    Problem 13: Implement basic single-digit handler

--- kotlin-leetcode/src/main/kotlin/dev/codefactory/leetcode/core/Solution013RomanToInteger.kt
+++ kotlin-leetcode/src/main/kotlin/dev/codefactory/leetcode/core/Solution013RomanToInteger.kt
@@ -1,7 +1,22 @@
 package dev.codefactory.leetcode.core
 
+import java.lang.IllegalArgumentException
+
 class Solution013RomanToInteger {
     fun romanToInt(s: String): Int {
+        if (s.length==1) return strToInt(s)
+
         return 0
+    }
+
+    private fun strToInt(s: String): Int = when(s) {
+        "I" -> 1
+        "V" -> 5
+        "X" -> 10
+        "L" -> 50
+        "C" -> 100
+        "D" -> 500
+        "M" -> 1000
+        else -> throw IllegalArgumentException("Unknown roman numeral $s")
     }
 }
