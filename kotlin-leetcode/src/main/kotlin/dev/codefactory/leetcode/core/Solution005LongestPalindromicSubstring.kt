ac2e04a8a3d3c53ff6140289fe50ae064fb0f53a
LM Bibera <secret.transaction@gmail.com>
2023-02-20 21:26:11 +0800

    Problem 5: Handle the easy tasks first

--- kotlin-leetcode/src/main/kotlin/dev/codefactory/leetcode/core/Solution005LongestPalindromicSubstring.kt
+++ kotlin-leetcode/src/main/kotlin/dev/codefactory/leetcode/core/Solution005LongestPalindromicSubstring.kt
@@ -2,6 +2,12 @@ package dev.codefactory.leetcode.core
 
 class Solution005LongestPalindromicSubstring {
     fun longestPalindrome(s: String): String {
+        if (s.length<=1) return s
+        if (s.length==2) {
+            return if (s[0]==s[1]) s
+            else s[0].toString()
+        }
+
         return s
     }
 }
