a7abfad027309357bed7b5394df60873855c0971
LM Bibera <secret.transaction@gmail.com>
2023-02-21 08:52:09 +0800

    Problem 7: Initial implementation

--- kotlin-leetcode/src/main/kotlin/dev/codefactory/leetcode/core/Solution007ReverseInt.kt
+++ kotlin-leetcode/src/main/kotlin/dev/codefactory/leetcode/core/Solution007ReverseInt.kt
@@ -2,6 +2,16 @@ package dev.codefactory.leetcode.core
 
 class Solution007ReverseInt {
     fun reverse(x: Int): Int {
-        return -1
+        var i = x
+        var reversed = 0
+        var remainder: Int
+
+        while (i != 0) {
+            remainder = i % 10
+            reversed = (reversed*10)+remainder
+            i /= 10
+        }
+
+        return reversed
     }
 }
