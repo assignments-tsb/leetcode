0ea6e098ac41859cde914f0467832a1bcb6eb122
LM Bibera <secret.transaction@gmail.com>
2023-02-21 10:17:05 +0800

    Problem 8: Initial implementation

--- kotlin-leetcode/src/main/kotlin/dev/codefactory/leetcode/core/Solution008StringToInt.kt
+++ kotlin-leetcode/src/main/kotlin/dev/codefactory/leetcode/core/Solution008StringToInt.kt
@@ -2,6 +2,22 @@ package dev.codefactory.leetcode.core
 
 class Solution008StringToInt {
     fun myAtoi(s: String): Int {
-        return Int.MAX_VALUE
+        if (s.isEmpty()) return 0
+
+        val isNegativeNumber = s.startsWith("-")
+        val absoluteStr = if (s.startsWith("-") || s.startsWith("+")) s.drop(1) else s
+
+        var value = 0
+        var multiplier = 1
+        for (c in absoluteStr) {
+            value = value*multiplier + c.digitToInt()
+            multiplier *= 10
+        }
+
+        if (isNegativeNumber) {
+            return -1*value
+        }
+
+        return value
     }
 }
