f01857a727fb70245ce2538ad5056b20f244ffc3
LM Bibera <secret.transaction@gmail.com>
2023-02-24 18:50:13 +0800

    Problem 12: Improve test cases

--- typescript-leetcode/src/solution_0012/intToRoman.test.tsx
+++ typescript-leetcode/src/solution_0012/intToRoman.test.tsx
@@ -1,5 +1,26 @@
 import intToRoman from './intToRoman'
 
-test('should convert int to roman', () => {
-    expect(intToRoman(1)).toBe("I");
-});
+describe('intToRoman', () => {
+    it.each([
+        [1, 'I'],
+        [5, 'V'],
+        [10, 'X'],
+        [50, 'L'],
+        [100, 'C'],
+        [500, 'D'],
+        [1000, 'M'],
+    ])('convert the integer %p into a single-character roman numeral %p', (num: number, result: string) => {
+        expect(intToRoman(num)).toBe(result);
+    });
+
+    it.each([
+        [4, 'IV'],
+        [9, 'IX'],
+        [40, 'XL'],
+        [90, 'XC'],
+        [400, 'CD'],
+        [900, 'CM'],
+    ])('convert the subtract the previous digit', (num: number, result: string) => {
+        expect(intToRoman(num)).toBe(result);
+    });
+})
