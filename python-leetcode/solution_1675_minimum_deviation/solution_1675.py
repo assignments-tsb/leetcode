336aa6baf56e056847cb855cd56fcf0d6ccddeb3
LM Bibera <secret.transaction@gmail.com>
2023-02-24 17:03:47 +0800

    Problem 1675: Add basic test case

---
+++ python-leetcode/solution_1675_minimum_deviation/solution_1675.py
@@ -0,0 +1,7 @@
+class Solution1675(object):
+    def minimumDeviation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return -1
---
+++ python-leetcode/solution_1675_minimum_deviation/test_solution_1675.py
@@ -0,0 +1,18 @@
+import unittest
+
+from solution_1675 import Solution1675
+
+
+class Solution1675TestCase(unittest.TestCase):
+    def test_basic_example(self):
+        solution = Solution1675()
+
+        nums = [1, 2, 3, 4]
+        expected_result = 1
+        actual_result = solution.minimumDeviation(nums)
+
+        self.assertEqual(actual_result, expected_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
