1f265f7d6e13d5ced5db90fdf0a5e777f0bba1ab
LM Bibera <secret.transaction@gmail.com>
2023-02-26 14:24:04 +0800

    Problem 12: Implement int-to-romain in typescript

--- typescript-leetcode/src/solution_0012/intToRoman.tsx
+++ typescript-leetcode/src/solution_0012/intToRoman.tsx
@@ -1,5 +1,18 @@
+
+let intValues = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+let strValues = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+
 function intToRoman(num: number): string {
-    return "I"
+    let roman = "";
+
+    for (let i=0; i<intValues.length; i++) {
+        while (num >= intValues[i]) {
+            num -= intValues[i]
+            roman += strValues[i]
+        }
+    }
+
+    return roman
 }
 
 export default intToRoman;
