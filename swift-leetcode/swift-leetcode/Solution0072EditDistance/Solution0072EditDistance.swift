348b89072711b85552c1495cd41c9fc1001cada8
LM Bibera <secret.transaction@gmail.com>
2023-02-26 17:56:04 +0800

    Code cleanup

--- swift-leetcode/swift-leetcode/Solution0072EditDistance/Solution0072EditDistance.swift
+++ swift-leetcode/swift-leetcode/Solution0072EditDistance/Solution0072EditDistance.swift
@@ -9,33 +9,6 @@ import Foundation
 
 class Solution0072EditDistance {
     
-    struct Matrix {
-        let rows: Int
-        let columns: Int
-        var data: [Int]
-        
-        init(rows: Int, columns: Int) {
-            self.rows = rows
-            self.columns = columns
-            self.data = Array(repeating: 0, count: rows * columns)
-        }
-        
-        func indexIsValid(row: Int, column: Int) -> Bool {
-            return row >= 0 && row < rows && column >= 0 && column < columns
-        }
-        
-        subscript(row: Int, column: Int) -> Int {
-            get {
-                assert(indexIsValid(row: row, column: column), "Index out of range")
-                return data[(row * columns) + column]
-            }
-            set {
-                assert(indexIsValid(row: row, column: column), "Index out of range")
-                data[(row * columns) + column] = newValue
-            }
-        }
-    }
-    
     func minDistance(_ word1: String, _ word2: String) -> Int {
         let m = word1.count
         let n = word2.count
@@ -52,12 +25,45 @@ class Solution0072EditDistance {
         
         for i in 1...m {
             for j in 1...n {
-//                if (word1.cha[i-1] == word2[j-1]) {
-//                    dp[i][j] = dp[i-1][j-1]
-//                }
+                if (word1[i-1] == word2[j-1]) {
+                    dp[i,j] = dp[i-1,j-1]
+                }
             }
         }
         
         return -1
+    }
+}
+
+struct Matrix {
+    let rows: Int
+    let columns: Int
+    var data: [Int]
+    
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        self.data = Array(repeating: 0, count: rows * columns)
+    }
+    
+    func indexIsValid(row: Int, column: Int) -> Bool {
+        return row >= 0 && row < rows && column >= 0 && column < columns
+    }
+    
+    subscript(row: Int, column: Int) -> Int {
+        get {
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            return data[(row * columns) + column]
+        }
+        set {
+            assert(indexIsValid(row: row, column: column), "Index out of range")
+            data[(row * columns) + column] = newValue
+        }
+    }
+}
+
+extension String {
+    subscript(i: Int) -> String {
+        return  i < count ? String(self[index(startIndex, offsetBy: i)]) : ""
     }
 }
