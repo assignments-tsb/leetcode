ba8180b12f803c7c3497c345298b6cd21620a5b2
LM Bibera <secret.transaction@gmail.com>
2023-02-26 14:35:13 +0800

    Problem 12: Implemented

--- java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0012IntToRoman.java
+++ java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0012IntToRoman.java
@@ -1,7 +1,18 @@
 package dev.codefactory.leetcode.core;
 
 public class Solution0012IntToRoman {
+    private static final int[] INT_VALUES = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final String[] STR_VALUES = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
     public String intToRoman(int num) {
-        return "";
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < INT_VALUES.length; i++) {
+            while (num >= INT_VALUES[i]) {
+                num -= INT_VALUES[i];
+                sb.append(STR_VALUES[i]);
+            }
+        }
+        return sb.toString();
     }
 }
