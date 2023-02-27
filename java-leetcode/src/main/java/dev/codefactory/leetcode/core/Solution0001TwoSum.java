334967f20eee1677a39e8ba7a658a8402d13e33f
LM Bibera <secret.transaction@gmail.com>
2023-02-27 08:13:15 +0800

    Problem 1: Implement basic solution for two-sum

--- java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0001TwoSum.java
+++ java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0001TwoSum.java
@@ -1,7 +1,20 @@
 package dev.codefactory.leetcode.core;
 
+import java.util.HashMap;
+
 public class Solution0001TwoSum {
     public int[] twoSum(int[] nums, int target) {
+        var itemMap = new HashMap<Integer, Integer>();
+
+        for(int i=0; i<nums.length; i++) {
+            var pair = target-nums[i];
+            if (itemMap.containsKey(pair)) {
+                return new int[]{itemMap.get(pair), i};
+            }
+
+            itemMap.put(nums[i], i);
+        }
+
         return new int[]{-1};
     }
 }
