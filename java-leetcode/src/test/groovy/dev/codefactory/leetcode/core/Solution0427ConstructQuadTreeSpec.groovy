157e807a37c281f3f2544a5c7b4537f29ec06fd6
LM Bibera <secret.transaction@gmail.com>
2023-02-27 15:03:00 +0800

    Problem 427: Add more test case

--- java-leetcode/src/test/groovy/dev/codefactory/leetcode/core/Solution0427ConstructQuadTreeSpec.groovy
+++ java-leetcode/src/test/groovy/dev/codefactory/leetcode/core/Solution0427ConstructQuadTreeSpec.groovy
@@ -28,4 +28,37 @@ class Solution0427ConstructQuadTreeSpec extends Specification {
         result == expectedAsNode
     }
 
+    def 'it should construct a new quad-tree from a complex 8x8 grid'() {
+
+        given:
+        int[][] grid = [
+                [1,1,1,1,0,0,0,0],
+                [1,1,1,1,0,0,0,0],
+                [1,1,1,1,1,1,1,1],
+                [1,1,1,1,1,1,1,1],
+                [1,1,1,1,0,0,0,0],
+                [1,1,1,1,0,0,0,0],
+                [1,1,1,1,0,0,0,0],
+                [1,1,1,1,0,0,0,0],
+        ]
+
+        and:
+        Node expectedAsNode = new Node(false, false)
+        expectedAsNode.topLeft = new Node(true, true)
+        expectedAsNode.topRight = new Node(true, false)
+        expectedAsNode.bottomLeft = new Node(true, true)
+        expectedAsNode.bottomRight = new Node(false, true)
+
+        expectedAsNode.topRight.topLeft = new Node(false, true)
+        expectedAsNode.topRight.topRight = new Node(false, true)
+        expectedAsNode.topRight.bottomLeft = new Node(true, true)
+        expectedAsNode.topRight.bottomRight = new Node(true, true)
+
+        when:
+        def result = solution.construct(grid)
+
+        then:
+        result == expectedAsNode
+    }
+
 }
