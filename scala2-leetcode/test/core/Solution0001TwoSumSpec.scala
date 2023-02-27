68c6235762099668dfa545c17587819a836b8ad5
LM Bibera <secret.transaction@gmail.com>
2023-02-27 10:23:04 +0800

    Problem 1: Add other test case

--- scala2-leetcode/test/core/Solution0001TwoSumSpec.scala
+++ scala2-leetcode/test/core/Solution0001TwoSumSpec.scala
@@ -5,7 +5,7 @@ import org.scalatestplus.play.PlaySpec
 class Solution0001TwoSumSpec extends PlaySpec {
 
   "Solution" must {
-    "return the indexes of two numbers whose sum is the target" in {
+    "return the indexes of two numbers whose sum is the target (Example 1)" in {
 
       val nums = Array(2,7,11,15)
       val target = 9
@@ -15,6 +15,29 @@ class Solution0001TwoSumSpec extends PlaySpec {
 
       result mustBe expectedResult
     }
+
+    "return the indexes of two numbers whose sum is the target (Example 2)" in {
+
+      val nums = Array(3,2,4)
+      val target = 6
+      val expectedResult = Array(1, 2)
+
+      val result = Solution0001TwoSum.twoSum(nums, target)
+
+      result mustBe expectedResult
+    }
+
+    "return the indexes of two numbers whose sum is the target (Example 3)" in {
+
+      val nums = Array(3, 3)
+      val target = 6
+      val expectedResult = Array(0, 1)
+
+      val result = Solution0001TwoSum.twoSum(nums, target)
+
+      result mustBe expectedResult
+    }
+
 
   }
 
