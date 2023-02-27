175ebf41dec368059d8e2f01a54c43e8d4fe1bd2
LM Bibera <secret.transaction@gmail.com>
2023-02-27 10:22:48 +0800

    Problem 1: Add initial implementation

--- scala2-leetcode/app/core/Solution0001TwoSum.scala
+++ scala2-leetcode/app/core/Solution0001TwoSum.scala
@@ -2,6 +2,10 @@ package core
 
 object Solution0001TwoSum {
   def twoSum(nums: Array[Int], target: Int): Array[Int] = {
-    Array(0,1)
+    val asMap: Map[Int, Int] = nums.zipWithIndex.toMap
+
+    asMap.find { case(value, index) => asMap.contains(target-value) && target-value!=index }
+      .map { case(value, index) => Array(index, asMap.get(target-value).get) }
+      .getOrElse(Array.empty)
   }
 }
