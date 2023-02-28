416129bf257cd9f7e423b7e26e076e436a651621
LM Bibera <secret.transaction@gmail.com>
2023-02-28 14:52:30 +0800

    Problem 98: Initial implementation

--- java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0098ValidBinarySearchTree.java
+++ java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0098ValidBinarySearchTree.java
@@ -2,6 +2,18 @@ package dev.codefactory.leetcode.core;
 
 public class Solution0098ValidBinarySearchTree {
     public boolean isValidBST(TreeNode root) {
-        return false;
+        if (root==null || root.left==null && root.right==null) {
+            return true;
+        }
+
+        if (root.left!=null && root.left.val>root.val) {
+            return false;
+        }
+
+        if (root.right!=null && root.right.val<root.val) {
+            return false;
+        }
+
+        return isValidBST(root.left) && isValidBST(root.right);
     }
 }
