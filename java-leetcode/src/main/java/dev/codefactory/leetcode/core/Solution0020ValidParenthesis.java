75c34c7f38f06a5df3a777517ff9c509f2439c99
LM Bibera <secret.transaction@gmail.com>
2023-02-28 14:26:50 +0800

    Problem 20: Add basic implementation

--- java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0020ValidParenthesis.java
+++ java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0020ValidParenthesis.java
@@ -1,7 +1,26 @@
 package dev.codefactory.leetcode.core;
 
+import java.util.List;
+import java.util.Stack;
+
 public class Solution0020ValidParenthesis {
+
+    private final static List<Character> OPEN_BRACKET = List.of('[', '{', '(');
+    private final static List<Character> CLOSE_BRACKET = List.of(']', '}', ')');
     public boolean isValid(String s) {
-        return false;
+        var stack = new Stack<Character>();
+
+        for (int i=0; i<s.length(); i++) {
+            var current = s.charAt(i);
+
+            if (OPEN_BRACKET.contains(current)) {
+                stack.push(current);
+            } else {
+                var partner = stack.pop();
+                if (CLOSE_BRACKET.indexOf(current) != OPEN_BRACKET.indexOf(partner)) return false;
+            }
+        }
+
+        return stack.isEmpty();
     }
 }
