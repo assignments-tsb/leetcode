cf680dc75fc321092706c2ebbe8e117fb8ce1b5c
LM Bibera <secret.transaction@gmail.com>
2023-02-28 16:53:23 +0800

    Problem 41: Basic implementation

--- java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0041FirstMissingPositive.java
+++ java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0041FirstMissingPositive.java
@@ -1,7 +1,21 @@
 package dev.codefactory.leetcode.core;
 
+import java.util.HashMap;
+
 public class Solution0041FirstMissingPositive {
     public int firstMissingPositive(int[] nums) {
-        return nums[0];
+        var maxValue = 0;
+        var hashMap = new HashMap<Integer, Boolean>();
+
+        for (int num : nums) {
+            hashMap.put(num, true);
+            maxValue = Math.max(maxValue, num);
+        }
+
+        for (int i=1; i<maxValue; i++) {
+            if (!hashMap.containsKey(i)) return i;
+        }
+
+        return maxValue+1;
     }
 }
