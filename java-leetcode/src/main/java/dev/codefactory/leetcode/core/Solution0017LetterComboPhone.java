f4a8fd626f875d98b16c637b0cdc1f7029a3a604
LM Bibera <secret.transaction@gmail.com>
2023-02-28 13:59:14 +0800

    Problem 17: Add basic recursive implementation

--- java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0017LetterComboPhone.java
+++ java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0017LetterComboPhone.java
@@ -1,5 +1,7 @@
 package dev.codefactory.leetcode.core;
 
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -16,6 +18,23 @@ public class Solution0017LetterComboPhone {
             '9', "wxyz"
     );
     public List<String> letterCombinations(String digits) {
-        return List.of();
+        if (digits.isEmpty()) return Collections.emptyList();
+
+        var combos = new LinkedList<String>();
+        createCombos(digits, 0, "", combos);
+        return combos;
+    }
+
+    private void createCombos(String digits, Integer index, String currentCombo, List<String> comboAccumulator) {
+        if (index == digits.length()) {
+            comboAccumulator.add(currentCombo);
+            return;
+        }
+
+        String characters = KEY_MAP.get(digits.charAt(index));
+
+        for (int i=0; i<characters.length(); i++) {
+            createCombos(digits, index+1, currentCombo + characters.charAt(i), comboAccumulator);
+        }
     }
 }
