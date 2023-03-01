60e0688bc8e5aa65ede5f5bea59b414e5da59b7d
LM Bibera <secret.transaction@gmail.com>
2023-03-01 08:43:30 +0800

    Problem 912: Initial attempt, but time limit exceeded

--- java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0912ArraySort.java
+++ java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0912ArraySort.java
@@ -2,6 +2,18 @@ package dev.codefactory.leetcode.core;
 
 public class Solution0912ArraySort {
     public int[] sortArray(int[] nums) {
+        if (nums.length<=1) return nums;
+
+        for (int i=0; i<nums.length; i++) {
+            for (int j=i+1; j<nums.length; j++) {
+                if (nums[j]<nums[i]) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+
         return nums;
     }
 }
