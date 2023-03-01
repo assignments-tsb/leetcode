9cbfcef2771c6d7e911dc49208f15e0bf1080629
LM Bibera <secret.transaction@gmail.com>
2023-03-01 09:06:11 +0800

    Problem 912: Improved speed but still is slow

--- java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0912ArraySort.java
+++ java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0912ArraySort.java
@@ -4,15 +4,38 @@ public class Solution0912ArraySort {
     public int[] sortArray(int[] nums) {
         if (nums.length<=1) return nums;
 
-        for (int i=0; i<nums.length; i++) {
-            for (int j=i+1; j<nums.length; j++) {
-                if (nums[j]<nums[i]) {
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                }
+        quicksort(nums,0, nums.length-1);
+
+        return nums;
+    }
+
+    private int[] quicksort(int[] nums, int left, int right) {
+        if (left >= right) return nums;
+
+        int pivot = nums[left];
+        int i = left+1;
+        int j = right;
+
+        while (i <= j) {
+            if (nums[i] > pivot && nums[j] <= pivot) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
+            } else if (nums[i] <= pivot) {
+                i++;
+            } else if (nums[j] > pivot) {
+                j--;
             }
         }
+
+        int temp = nums[left];
+        nums[left] = nums[j];
+        nums[j] = temp;
+
+        quicksort(nums, left, j-1);
+        quicksort(nums, j+1, right);
 
         return nums;
     }
