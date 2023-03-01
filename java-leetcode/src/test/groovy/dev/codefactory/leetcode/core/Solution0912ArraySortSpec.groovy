36bb01683b65ebcc223121baf4554a596e2d626b
LM Bibera <secret.transaction@gmail.com>
2023-03-01 09:29:17 +0800

    Problem 912: Add speed test

--- java-leetcode/src/test/groovy/dev/codefactory/leetcode/core/Solution0912ArraySortSpec.groovy
+++ java-leetcode/src/test/groovy/dev/codefactory/leetcode/core/Solution0912ArraySortSpec.groovy
@@ -22,4 +22,23 @@ class Solution0912ArraySortSpec extends Specification {
         [3,5,6,0,1]                                                 | [0,1,3,5,6]
         [Integer.MAX_VALUE, 1, 5, 7, -1, 0, Integer.MIN_VALUE]      | [Integer.MIN_VALUE, -1, 0, 1, 5, 7, Integer.MAX_VALUE]
     }
+
+    def 'it should sort the array really fast'() {
+
+        given: 'a large array'
+        int[] largeArray = new int[50_000]
+        largeArray[0] = Integer.MAX_VALUE
+        largeArray[largeArray.length-1] = Integer.MIN_VALUE
+        for (int i=1; i<largeArray.length-1; i++) {
+            largeArray[i] = new Random().nextInt(500_000_000, Integer.MAX_VALUE)
+        }
+
+        when:
+        def output = solution.sortArray(largeArray)
+
+        then:
+        output.length == 50_000
+        output[0] == Integer.MIN_VALUE
+        output[output.length-1] == Integer.MAX_VALUE
+    }
 }
