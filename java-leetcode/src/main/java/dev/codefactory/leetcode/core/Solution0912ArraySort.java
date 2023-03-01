b1685f9d5f72a411e851faa3831f008555cd7a57
LM Bibera <secret.transaction@gmail.com>
2023-03-01 09:35:44 +0800

    Problem 912: Add merge sort solution

--- java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0912ArraySort.java
+++ java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0912ArraySort.java
@@ -4,11 +4,57 @@ public class Solution0912ArraySort {
     public int[] sortArray(int[] nums) {
         if (nums.length<=1) return nums;
 
-        quicksort(nums,0, nums.length-1);
+        mergeSort(nums,0, nums.length-1);
 
         return nums;
     }
 
+    private void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+
+    private void merge(int[] arr, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[l + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = arr[m + 1 + j];
+        }
+        int i = 0;
+        int j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    @Deprecated
     private int[] quicksort(int[] nums, int left, int right) {
         if (left >= right) return nums;
 
