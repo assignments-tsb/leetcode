ef8788f146ee7e879cc9262b2648b6a9c95ec8fb
LM Bibera <secret.transaction@gmail.com>
2023-03-02 14:09:40 +0800

    Problem 443: Implement

--- java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0443StringCompression.java
+++ java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0443StringCompression.java
@@ -1,7 +1,37 @@
 package dev.codefactory.leetcode.core;
 
 public class Solution0443StringCompression {
+
+    private static final char DEFAULT = ' ';
     public int compress(char[] chars) {
-        return 1;
+        StringBuilder s = new StringBuilder();
+
+        char current = DEFAULT;
+        int count = 0;
+
+        for (int i=0; i<chars.length; i++) {
+            if (current == chars[i]) {
+                count++;
+            } else {
+                if (count>1) {
+                    s.append(count);
+                }
+                count = 1;
+                s.append(chars[i]);
+                current = chars[i];
+            }
+        }
+
+        if (count>1) {
+            s.append(count);
+        }
+
+        for (int i=0; i<chars.length; i++) {
+            if (i<s.length()) {
+                chars[i] = s.charAt(i);
+            }
+        }
+
+        return s.length();
     }
 }
