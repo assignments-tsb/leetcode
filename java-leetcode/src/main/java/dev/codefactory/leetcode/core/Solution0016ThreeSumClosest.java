7aa4d77578c9ae75c02d64f101eec81df685aa24
LM Bibera <secret.transaction@gmail.com>
2023-03-02 14:47:29 +0800

    Problem 16: Initial implementation

--- java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0016ThreeSumClosest.java
+++ java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0016ThreeSumClosest.java
@@ -1,7 +1,31 @@
 package dev.codefactory.leetcode.core;
 
+import java.util.Arrays;
+
 public class Solution0016ThreeSumClosest {
     public int threeSumClosest(int[] nums, int target) {
-        return -1;
+        Arrays.sort(nums);
+
+        int closest = Integer.MAX_VALUE;
+
+        for (int i=0; i<nums.length-2; i++) {
+
+            int left = i+1;
+            int right = nums.length-1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum-target < closest-target) {
+                    closest = sum;
+                }
+
+                left++;
+                right--;
+            }
+
+        }
+
+        return closest;
     }
 }
