6aab7334a40f45be663bd2784388398555f126a6
LM Bibera <secret.transaction@gmail.com>
2023-03-03 08:19:57 +0800

    Problem 33: WIP, no time to solve

--- java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0033SearchInRotatedArray.java
+++ java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0033SearchInRotatedArray.java
@@ -2,6 +2,16 @@ package dev.codefactory.leetcode.core;
 
 public class Solution0033SearchInRotatedArray {
     public int search(int[] nums, int target) {
+        //find the pivot
+        int right = 0;
+        int left = nums.length-1;
+        int mid = (left + right) / 2;
+
+        while (left < right) {
+
+        }
+
+        //binary search
         return -1;
     }
 }
