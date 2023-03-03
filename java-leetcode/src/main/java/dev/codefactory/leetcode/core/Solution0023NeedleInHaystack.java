7d8162fc38bfab0351d3ec831b5502d42b2c52f4
LM Bibera <secret.transaction@gmail.com>
2023-03-03 08:26:19 +0800

    Problem 23: Initial implementation

--- java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0023NeedleInHaystack.java
+++ java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0023NeedleInHaystack.java
@@ -2,6 +2,19 @@ package dev.codefactory.leetcode.core;
 
 public class Solution0023NeedleInHaystack {
     public int strStr(String haystack, String needle) {
+
+        haystack:
+        for (int i=0; i<=haystack.length()-needle.length(); i++) {
+            if (haystack.charAt(i) == needle.charAt(0)) {
+                for (int j=0; j<needle.length(); j++) {
+                    if (haystack.charAt(i+j) != needle.charAt(j)) {
+                        continue haystack;
+                    }
+                }
+                return i;
+            }
+        }
+
         return -1;
     }
 }
