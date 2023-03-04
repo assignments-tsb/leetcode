e0cb007d569df4e1040fddee8b753dcf0ab8c2ad
LM Bibera <secret.transaction@gmail.com>
2023-03-04 14:38:54 +0800

    Problem 2444: Add more test case

--- java-leetcode/src/test/groovy/dev/codefactory/leetcode/core/Solution2444CountSubArraysWithBoundsSpec.groovy
+++ java-leetcode/src/test/groovy/dev/codefactory/leetcode/core/Solution2444CountSubArraysWithBoundsSpec.groovy
@@ -4,6 +4,7 @@ import spock.lang.Specification
 
 class Solution2444CountSubArraysWithBoundsSpec extends Specification {
 
+    def static LARGE = [35054,398719,945315,945315,820417,945315,35054,945315,171832,945315,35054,109750,790964,441974,552913]
     def solution = new Solution2444CountSubArraysWithBounds()
 
     def 'it should count the number of sub-arrays matching the bound'(int[] array, int minK, int maxK, int expected) {
@@ -15,8 +16,10 @@ class Solution2444CountSubArraysWithBoundsSpec extends Specification {
         result == expected
 
         where:
-        array           | minK  | maxK  | expected
-        [1,3,5,2,7,5]   | 1     | 5     | 2
-        [1,1,1,1]       | 1     | 1     | 10
+        array           | minK  | maxK   | expected
+        [1,3,5,2,7,5]   | 1     | 5      | 2
+        [1,1,1,1]       | 1     | 1      | 10
+        LARGE           | 35054 | 945315 | 81
     }
+
 }
