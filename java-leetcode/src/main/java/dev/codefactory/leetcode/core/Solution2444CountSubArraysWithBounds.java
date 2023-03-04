b5641420982e4b50c357ca383a91edfa7dc80ffe
LM Bibera <secret.transaction@gmail.com>
2023-03-04 21:06:51 +0800

    Problem 2444: Fix time limit issue

--- java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution2444CountSubArraysWithBounds.java
+++ java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution2444CountSubArraysWithBounds.java
@@ -2,54 +2,27 @@ package dev.codefactory.leetcode.core;
 
 public class Solution2444CountSubArraysWithBounds {
     public long countSubarrays(int[] nums, int minK, int maxK) {
-        if (minK==maxK) return countIfMinEqualsMax(nums, minK);
-        return countWithNormal(nums, minK, maxK);
-    }
-
-    private long countWithNormal(int[] nums, int minK, int maxK) {
-        int count = 0;
-
-        for (int i=0; i<nums.length; i++) {
-            int min = Integer.MAX_VALUE;
-            int max = Integer.MIN_VALUE;
-
-            for (int j=i; j<nums.length; j++) {
-                if (nums[j] > max) max = nums[j];
-                if (nums[j] < min) min = nums[j];
-
-                if (min==minK && max==maxK) {
-                    count++;
-                }
-
-                if (max > maxK || min < minK) break;
+        int n = nums.length;
+        int startInd = 0;
+        int lastMinInd = -1;
+        int lastMaxInd = -1;
+        long res = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < minK || nums[i] > maxK) {
+                lastMinInd = lastMaxInd = -1;
+                startInd = i + 1;
             }
+
+            if (nums[i] == minK) {
+                lastMinInd = i;
+            }
+
+            if (nums[i] == maxK) {
+                lastMaxInd = i;
+            }
+
+            res += Math.max(0L, Math.min(lastMinInd, lastMaxInd) - startInd + 1);
         }
-        return count;
+        return res;
     }
-
-    private long countIfMinEqualsMax(int[] num, int k) {
-        long count = 0;
-
-        int left = 0;
-        int i = 0;
-        while (i<num.length) {
-            if (num[i]!=k) {
-                count += factorial(i-left+1);
-                left = i+1;
-            }
-            if (i==num.length-1) {
-                count += factorial(i-left+2);
-            }
-            i++;
-        }
-
-        return count;
-    }
-
-    private long factorial(long n) {
-        long sum = 0;
-        for (int i=1; i<n; i++) sum+=i;
-        return sum;
-    }
-
 }
