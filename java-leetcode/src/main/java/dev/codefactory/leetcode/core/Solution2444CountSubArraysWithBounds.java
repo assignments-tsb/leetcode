bd7235eaa9b45a05fd07eca5cb5761d5fef8ada6
LM Bibera <secret.transaction@gmail.com>
2023-03-04 14:34:38 +0800

    Problem 2444: Initial implementation

--- java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution2444CountSubArraysWithBounds.java
+++ java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution2444CountSubArraysWithBounds.java
@@ -2,14 +2,17 @@ package dev.codefactory.leetcode.core;
 
 public class Solution2444CountSubArraysWithBounds {
     public long countSubarrays(int[] nums, int minK, int maxK) {
-        long count = 0;
+        int count = 0;
 
         for (int i=0; i<nums.length; i++) {
+            int min = nums[i];
+            int max = nums[i];
             for (int j=i; j<nums.length; j++) {
-                if (nums[j]>=minK && nums[j]<=maxK) {
+                if (nums[j] > max) max = nums[j];
+                if (nums[j] < min) max = nums[j];
+
+                if (min==minK && max==maxK) {
                     count++;
-                } else {
-                    break;
                 }
             }
         }
