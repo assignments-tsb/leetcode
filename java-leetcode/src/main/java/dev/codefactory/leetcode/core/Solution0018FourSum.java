c57bc3a13f75c5b943fa8a8b93863c7e9080673d
LM Bibera <secret.transaction@gmail.com>
2023-03-04 21:21:07 +0800

    Problem 18: Add test case

---
+++ java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0018FourSum.java
@@ -0,0 +1,10 @@
+package dev.codefactory.leetcode.core;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Solution0018FourSum {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        return Collections.emptyList();
+    }
+}
---
+++ java-leetcode/src/test/groovy/dev/codefactory/leetcode/core/Solution0018FourSumSpec.groovy
@@ -0,0 +1,34 @@
+package dev.codefactory.leetcode.core
+
+import spock.lang.Specification
+
+class Solution0018FourSumSpec extends Specification {
+
+    def solution = new Solution0018FourSum()
+
+    def 'it should return an array of unique quadruplet'(int[] nums, int target, List<List<Integer>> output) {
+
+        when:
+        def result = solution.fourSum(nums, target)
+
+        then:
+        equalsUnordered(output, result)
+
+        where:
+        nums                | target    | output
+        [1,0,-1,0,-2,2]     | 0         | [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+        [2,2,2,2,2]         | 8         | [[2,2,2,2]]
+    }
+
+    private boolean equalsUnordered(List<List<Integer>> lists1, List<List<Integer>> lists2) {
+        for (List<Integer> l1 : lists1) {
+            boolean found = false
+            for (List<Integer> l2 : lists2) {
+                if (l1.containsAll(l2)) {
+                    found = true
+                }
+            }
+            if (!found) return false
+        }
+    }
+}
