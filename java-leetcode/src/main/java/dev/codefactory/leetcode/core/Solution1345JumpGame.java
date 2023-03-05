097e24b9e3957c58f634fabe15d733bc6d2fbb3e
LM Bibera <secret.transaction@gmail.com>
2023-03-05 09:38:24 +0800

    Problem 1345: Fix bugs

--- java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution1345JumpGame.java
+++ java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution1345JumpGame.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Solution1345JumpGame {
     public int minJumps(int[] arr) {
-        if (arr.length==1) return 1;
+        if (arr.length==1) return 0;
 
         Map<Integer, Integer> minSteps = new HashMap<>();
         minSteps.put(0,0);
@@ -14,21 +14,21 @@ public class Solution1345JumpGame {
 
         while (!q.isEmpty()) {
             var currentIndex = q.remove();
-            var currentStep = minSteps.get(0);
+            var currentStep = minSteps.get(currentIndex);
 
-            //left
+            //explore forward
             if (currentIndex+1<arr.length && !minSteps.containsKey(currentIndex+1)) {
                 minSteps.put(currentIndex+1, currentStep+1);
                 q.add(currentIndex+1);
             }
 
-            //right
+            //explore backward
             if (currentIndex-1>=0 && !minSteps.containsKey(currentIndex-1)) {
                 minSteps.put(currentIndex-1, currentStep+1);
                 q.add(currentIndex-1);
             }
 
-            //special
+            //special jump to equals
             for (int i=0; i<arr.length; i++) {
                 if (arr[currentIndex]==arr[i] && i!=currentIndex && !minSteps.containsKey(i)) {
                     minSteps.put(i, currentStep+1);
