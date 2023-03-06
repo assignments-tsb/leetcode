9274d7d4809a6bc3d06a7ae9b4b129b3fa314a9b
LM Bibera <secret.transaction@gmail.com>
2023-03-06 21:11:42 +0800

    Problem 25: Final implementation

--- java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0025ReverseNodes.java
+++ java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0025ReverseNodes.java
@@ -7,23 +7,30 @@ import java.util.Queue;
 
 public class Solution0025ReverseNodes {
     public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null) return null;
+
         Queue<ListNode> q = new LinkedList<>();
         var current = head;
 
-        var i = k;
-        while (current!=null && i>0) {
+        for (int i=0; i<k; i++) {
+            // we don't have enough nodes to reverse,
+            // keep the original
+            if (current==null) return head;
+
             q.offer(current);
             current = current.next;
-            i--;
         }
+
+        var finalNode = q.remove();
+        finalNode.next = reverseKGroup(current, k);
 
         while (!q.isEmpty()) {
             var top = q.remove();
-            top.next = current;
-            current = top;
+            top.next = finalNode;
+            finalNode = top;
         }
 
-        return current;
+        return finalNode;
     }
 
 }
--- java-leetcode/src/test/groovy/dev/codefactory/leetcode/core/Solution0025ReverseNodesSpec.groovy
+++ java-leetcode/src/test/groovy/dev/codefactory/leetcode/core/Solution0025ReverseNodesSpec.groovy
@@ -17,6 +17,7 @@ class Solution0025ReverseNodesSpec extends Specification {
 
         where:
         input       | k | expected
+        [1,2,3,4,5] | 6 | [1,2,3,4,5]
         [1,2,3,4,5] | 2 | [2,1,4,3,5]
         [1,2,3,4,5] | 3 | [3,2,1,4,5]
         [1,2,3,4,5] | 4 | [4,3,2,1,5]
