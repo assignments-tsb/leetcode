bc95124b18b13f2ed50a9e1525d55c8f5f05e189
LM Bibera <secret.transaction@gmail.com>
2023-03-06 21:40:11 +0800

    Problem 35: Initial implementation

--- java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0035SearchInsertPosition.java
+++ java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0035SearchInsertPosition.java
@@ -2,6 +2,23 @@ package dev.codefactory.leetcode.core;
 
 public class Solution0035SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
-        return -1;
+        int n = nums.length-1;
+        int left = 0;
+        int right = n;
+        int mid = n/2;
+
+        while (left <= right) {
+            if (nums[mid]==target) return mid;
+
+            mid = (left + right) / 2;
+
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
     }
 }
