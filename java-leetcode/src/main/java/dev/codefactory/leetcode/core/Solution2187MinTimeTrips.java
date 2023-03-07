84d9b554a886d77a3ffa9464da23a943bf72ddf3
LM Bibera <secret.transaction@gmail.com>
2023-03-07 11:57:30 +0800

    Problem 2187: Initial solution (but slow)

--- java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution2187MinTimeTrips.java
+++ java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution2187MinTimeTrips.java
@@ -2,6 +2,12 @@ package dev.codefactory.leetcode.core;
 
 public class Solution2187MinTimeTrips {
     public long minimumTime(int[] time, int totalTrips) {
-        return Long.MIN_VALUE;
+        for (int t=0; true; t++) {
+            int totalTripsAtT = 0;
+            for (int i : time) {
+                totalTripsAtT += t / i;
+            }
+            if (totalTripsAtT>=totalTrips) return t;
+        }
     }
 }
