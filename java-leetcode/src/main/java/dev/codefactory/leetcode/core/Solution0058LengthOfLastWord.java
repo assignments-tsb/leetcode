ad8649560b85f0a9bfb0321e9ec5c4495ca63c8a
LM Bibera <secret.transaction@gmail.com>
2023-03-08 17:57:14 +0800

    Problem 58: Implement

--- java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0058LengthOfLastWord.java
+++ java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0058LengthOfLastWord.java
@@ -2,6 +2,19 @@ package dev.codefactory.leetcode.core;
 
 public class Solution0058LengthOfLastWord {
     public int lengthOfLastWord(String s) {
-        return -1;
+        var startCount = false;
+        var count = 0;
+        for (int i=s.length()-1; i>=0; i--) {
+            if (s.charAt(i)!=' ') {
+                count++;
+            }
+            if (startCount && s.charAt(i)==' ') {
+                break;
+            }
+            if (!startCount && s.charAt(i)!=' ') {
+                startCount = true;
+            }
+        }
+        return count;
     }
 }
