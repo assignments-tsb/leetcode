de7dc4c122aa96f883153138ed578c2b52b0c3b7
LM Bibera <secret.transaction@gmail.com>
2023-03-09 08:43:06 +0800

    Problem 83: Tests

--- java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0083RemoveDuplicateNodes.java
+++ java-leetcode/src/main/java/dev/codefactory/leetcode/core/Solution0083RemoveDuplicateNodes.java
@@ -4,6 +4,22 @@ import dev.codefactory.leetcode.core.common.ListNode;
 
 public class Solution0083RemoveDuplicateNodes {
     public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode prev = head;
+        ListNode curr = head.next;
+
+        while (curr != null) {
+            if (prev.val == curr.val) {
+                prev.next = curr.next;
+            } else {
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+
         return head;
     }
 }
